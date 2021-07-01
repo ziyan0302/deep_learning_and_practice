fcefdfcd2cf8ee6ce23b723fd5b90bef4fa0d3e7
ziyan <j20130230@gmail.com>
2021-07-01 14:51:12 +0800

    Lab6

---
+++ Lab6/tensorboard_test.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from packaging import version
+
+import tensorflow as tf
+from tensorflow import keras
+
+print("TensorFlow version: ", tf.__version__)
+assert version.parse(tf.__version__).release[0] >= 2, \
+    "This notebook requires TensorFlow 2.0 or above."
